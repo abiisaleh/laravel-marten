41eb7722e46f150792cc9a160a50c68d1662f66a
abiisaleh <abi.saleh.25@gmail.com>
2023-10-24 10:08:20 +0900

    add 404 image

--- resources/views/pages/rekomendasi404.blade.php
+++ resources/views/pages/rekomendasi404.blade.php
@@ -4,7 +4,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-12">
         <div class="mx-auto max-w-screen-sm text-center">
-            <img class="mt-8 mb-4 mx-auto" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/404/404-computer.svg" alt="404 Not Found">
+            <img class="mt-8 mb-4 mx-auto" src="/404-computer.svg" alt="404 Not Found">
 
             <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Data tidak ditemukan.</p>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Maaf, rekomendasi tidak bisa diberikan karena data yang anda cari tidak ditemukan. </p>
